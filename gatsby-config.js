4e7a67bf5a97cf51fc5a967ddd514c5772c01891
Allan Glasier <allan@phoenixdesign.ca>
2021-05-15 14:10:04 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "EnRoute Productions",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
